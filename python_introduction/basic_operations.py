4d2cbfb9c47205615a48907306cda99564f0783d
Phantom <ramonadedotun550@gmail.com>
2025-05-23 12:28:51 +0100

    Updated Task 0

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,9 +1,9 @@
-num1 = 10
-num2 = 5
-sum_result = num1 + num2
-substract_result = num1 - num2
-multiply_result = num1 * num2
+number1 = 10
+number2 = 5
+sum_result = number1 + number2
+substract_result = number1 - number2
+multiply_result = number1 * number2
 
-print(f"Addition of {num1} and {num2} is {sum_result}")
-print(f"Subtraction of {num1} and {num2} is {substract_result}")
-print(f"Multiplication of {num1} and {num2} is {multiply_result}")
+print(f"Addition of {number1} and {number2} is {sum_result}")
+print(f"Subtraction of {number1} and {number2} is {substract_result}")
+print(f"Multiplication of {number1} and {number2} is {multiply_result}")
